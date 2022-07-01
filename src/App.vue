094383e9a58bd89c0b079da911a6ec2124cb2e2b
DriftKingTW <driftkingtw@gmail.com>
2022-07-01 11:34:09 +0800

    build: add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
+  <v-app>
+    <v-app-bar app color="primary" dark dense flat clipped-right>
+      <div class="d-flex align-center">
+        <v-toolbar-title> Pi Dash </v-toolbar-title>
+      </div>
+      <span v-if="debug" class="ml-4 font-weight-black warning--text">
+        <v-icon small left color="warning">
+          mdi-application-brackets-outline
+        </v-icon>
+        Debug Mode
+      </span>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
-  </div>
+
+    <v-main>
+      <router-view :key="routerViewKey" />
+    </v-main>
+  </v-app>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
