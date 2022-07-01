567d6145dfc765f2f75df9a382a74078d4cefad4
DriftKingTW <driftkingtw@gmail.com>
2022-07-01 14:03:30 +0800

    feat: add twitter statistics component

--- src/components/blocks/TwitterStatistics.vue
+++ src/components/blocks/TwitterStatistics.vue
@@ -1,11 +1,29 @@
 <template>
-  <v-card flat>
-    <v-card-title>Statistics</v-card-title>
-    <v-card-text> Test </v-card-text>
+  <v-card flat :href="twitterUrl" target="_blank" :loading="loading">
+    <v-card-title>Twitter Statistics</v-card-title>
+    <v-card-text>
+      <v-row align="center" class="spacer" no-gutters>
+        <v-col cols="3">
+          <v-avatar color="primary" max-width="100%" right>
+            <img :src="data.profile_image_url" :alt="data.name" />
+          </v-avatar>
+        </v-col>
+        <v-col cols="9">
+          <strong v-html="data.name"></strong>
+          <v-divider></v-divider>
+          <v-icon small left>mdi-account</v-icon>
+          {{ data.public_metrics ? data.public_metrics.followers_count : "" }}
+          followers
+          <br />
+        </v-col>
+      </v-row>
+    </v-card-text>
   </v-card>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   components: {
     //
@@ -13,7 +31,8 @@ export default {
 
   data() {
     return {
-      //
+      loading: true,
+      data: {},
     };
   },
 
@@ -22,13 +41,24 @@ export default {
   },
 
   methods: {
-    initialize() {
-      //
+    async initialize() {
+      this.loading = true;
+      try {
+        const res = await axios.get(
+          `${process.env.VUE_APP_API_URL}/twitter/statistics`
+        );
+        this.data = { ...res.data };
+      } catch (e) {
+        console.log(e);
+      }
+      this.loading = false;
     },
   },
 
   computed: {
-    //
+    twitterUrl: function () {
+      return `https://twitter.com/${this.data.username}`;
+    },
   },
 };
 </script>
