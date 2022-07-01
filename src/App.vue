2d5c73e092d0cf4cc8ce3b3c22476598b4b1e525
DriftKingTW <driftkingtw@gmail.com>
2022-07-01 11:34:36 +0800

    style: remove unused code

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,6 @@
       <div class="d-flex align-center">
         <v-toolbar-title> Pi Dash </v-toolbar-title>
       </div>
-      <span v-if="debug" class="ml-4 font-weight-black warning--text">
-        <v-icon small left color="warning">
-          mdi-application-brackets-outline
-        </v-icon>
-        Debug Mode
-      </span>
       <v-spacer></v-spacer>
     </v-app-bar>
 
