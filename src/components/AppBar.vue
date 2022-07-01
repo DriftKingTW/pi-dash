6f2bb6bc2afc271f88d31220834b757c7591c557
DriftKingTW <driftkingtw@gmail.com>
2022-07-01 14:14:32 +0800

    ui: update nav bar icon

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -2,8 +2,14 @@
   <v-app-bar app color="primary" dark dense flat>
     <div class="d-flex align-center">
       <v-toolbar-title>
-        <v-icon left>mdi-gauge</v-icon>
-        Pi Dash
+        <v-app-bar-nav-icon>
+          <raspberry-pi-icon
+            size="1.5x"
+            fill="white"
+            class="mr-2"
+          ></raspberry-pi-icon>
+        </v-app-bar-nav-icon>
+        <strong>Pi Dash</strong>
       </v-toolbar-title>
     </div>
     <v-spacer></v-spacer>
@@ -11,9 +17,11 @@
 </template>
 
 <script>
+import { RaspberryPiIcon } from "vue-simple-icons";
+
 export default {
   components: {
-    //
+    RaspberryPiIcon,
   },
 
   data() {
