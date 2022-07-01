dfa543d35ffc75a2fda606aeaef41aa740c008d7
DriftKingTW <driftkingtw@gmail.com>
2022-07-01 14:10:25 +0800

    ui: add icon for nav bar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app-bar app color="primary" dark dense flat>
     <div class="d-flex align-center">
-      <v-toolbar-title> Pi Dash </v-toolbar-title>
+      <v-toolbar-title>
+        <v-icon left>mdi-gauge</v-icon>
+        Pi Dash
+      </v-toolbar-title>
     </div>
     <v-spacer></v-spacer>
   </v-app-bar>
