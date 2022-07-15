446eb84a4984852bc7607ce4bb4448144efb684f
DriftKingTW <driftkingtw@gmail.com>
2022-07-15 13:48:14 +0800

    feat: update twitter block to social block

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="grid-container">
     <CalendarClock class="clock-block" />
-    <TwitterStatistics class="twitter-block" />
+    <SocialStatistics class="social-block" />
     <FamilyMartTracking class="family-mart-block" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import TwitterStatistics from "@/components/blocks/TwitterStatistics.vue";
+import SocialStatistics from "@/components/blocks/SocialStatistics.vue";
 import FamilyMartTracking from "@/components/blocks/FamilyMartTracking.vue";
 import CalendarClock from "@/components/blocks/CalendarClock.vue";
 
 export default {
   name: "HomeView",
-  components: { TwitterStatistics, FamilyMartTracking, CalendarClock },
+  components: { SocialStatistics, FamilyMartTracking, CalendarClock },
 
   computed: {},
 };
@@ -29,12 +29,12 @@ export default {
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 2;
   grid-template-areas:
-    "twitter family-mart-block clock"
-    "twitter family-mart-block clock";
+    "social family-mart-block clock"
+    "social family-mart-block clock";
 }
 
-.twitter-block {
-  grid-area: twitter;
+.social-block {
+  grid-area: social;
 }
 
 .family-mart-block {
