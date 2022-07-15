4f8b37bba7b5036ee1edb013b2342d2ab6aaa17b
DriftKingTW <driftkingtw@gmail.com>
2022-07-15 09:44:14 +0800

    feat: change about page to test page

---
+++ src/views/TestView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Test Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    //
+  },
+
+  data() {
+    return {
+      //
+    };
+  },
+
+  mounted() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      //
+    },
+  },
+
+  computed: {
+    //
+  },
+};
+</script>
