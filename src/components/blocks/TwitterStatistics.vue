253d34dfed9589279fd4006f8ebde2f33fe356e9
DriftKingTW <driftkingtw@gmail.com>
2022-07-15 13:47:20 +0800

    feat: update twitter block to social block

--- src/components/blocks/TwitterStatistics.vue
+++ src/components/blocks/TwitterStatistics.vue
@@ -1,36 +1,43 @@
 <template>
   <v-card flat :href="twitterUrl" target="_blank" :loading="loading">
     <v-card-title>
-      <twitter-icon size="1x" fill="white" class="mr-2"></twitter-icon>
-      Twitter Statistics
+      <v-icon left>mdi-account-group-outline</v-icon>
+      Social Statistics
     </v-card-title>
     <v-card-text>
-      <v-row align="center" class="spacer" no-gutters>
-        <v-col cols="3">
-          <v-avatar color="primary" max-width="100%" right>
-            <img :src="data.profile_image_url" :alt="data.name" />
-          </v-avatar>
-        </v-col>
-        <v-col cols="9">
-          <strong v-html="data.name"></strong>
-          <v-divider></v-divider>
-          <v-icon small left>mdi-account</v-icon>
-          {{ data.public_metrics ? data.public_metrics.followers_count : "" }}
-          followers
-          <br />
-        </v-col>
-      </v-row>
+      <v-list two-line>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img :src="data.profile_image_url" :alt="data.name"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ data.name }}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              <!-- <twitter-icon size="1x" fill="white" class="mr-1"> </twitter-icon> -->
+              <v-icon left small>mdi-twitter</v-icon>
+              {{
+                data.public_metrics ? data.public_metrics.followers_count : ""
+              }}
+              followers
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider :inset="true"></v-divider>
+      </v-list>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
 import axios from "axios";
-import { TwitterIcon } from "vue-simple-icons";
+// import { TwitterIcon } from "vue-simple-icons";
 
 export default {
   components: {
-    TwitterIcon,
+    // TwitterIcon,
   },
 
   data() {
