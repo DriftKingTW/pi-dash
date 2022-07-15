f1a5eea21813456c0efe1d61ba0706d702f9fbb9
DriftKingTW <driftkingtw@gmail.com>
2022-07-15 13:27:04 +0800

    feat: add family mart package tracking block

---
+++ src/components/blocks/FamilyMartTracking.vue
@@ -0,0 +1,138 @@
+<template>
+  <v-card flat :loading="loading">
+    <v-card-title>
+      <v-icon left>mdi-package-variant-closed</v-icon>
+      Family Mart Package Tracking
+      <v-spacer></v-spacer>
+      <v-btn icon @click="initialize" :disabled="loading">
+        <v-icon>mdi-refresh</v-icon>
+      </v-btn>
+    </v-card-title>
+    <v-card-subtitle>Updated at {{ lastUpdate }}</v-card-subtitle>
+    <v-card-text>
+      <v-text-field
+        label="Tracking ID"
+        v-model="trackingId"
+        dense
+        append-outer-icon="mdi-plus"
+        color="white"
+        @click:append-outer="addPackage"
+        @keyup.enter="addPackage"
+      >
+      </v-text-field>
+      <v-row align="center" class="spacer" no-gutters>
+        <v-col cols="12" v-for="(d, i) in data" :key="i">
+          <v-skeleton-loader
+            v-if="loading"
+            class="mx-auto"
+            width="100%"
+            type="paragraph"
+          ></v-skeleton-loader>
+          <v-chip
+            v-if="d.orderId"
+            small
+            label
+            color="primary lighten-1"
+            class="mb-2"
+            close
+            @click:close="removePackage(i)"
+          >
+            <v-icon small left>mdi-truck-cargo-container</v-icon>
+            {{ d.orderId }} {{ d.status }} {{ d.receiveDate }}
+          </v-chip>
+          <v-chip v-else small label color="primary lighten-1" class="mb-2">
+            <v-icon small left>mdi-emoticon-sad</v-icon>
+            No Data
+          </v-chip>
+          <br />
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  components: {
+    //
+  },
+
+  data() {
+    return {
+      loading: true,
+      trackingId: "",
+      packages: [],
+      data: [],
+      lastUpdate: new Date().toLocaleString(),
+    };
+  },
+
+  mounted() {
+    this.initialize();
+    setInterval(() => {
+      this.initialize();
+    }, 1000 * 60 * 30 /* 10 minutes */);
+  },
+
+  methods: {
+    async initialize() {
+      this.loading = true;
+      this.data = [];
+      this.packages = [];
+      this.packages = this.loadPackages();
+      for (const pkg of this.packages) {
+        try {
+          const query = {
+            trackingId: pkg,
+          };
+          const res = await axios.post(
+            `${process.env.VUE_APP_API_URL}/package/familymart`,
+            query
+          );
+          if (res.data.statusCode === "999") {
+            res.data.latestStatus.status = "查無訂單資料";
+          }
+          this.data = [...this.data, { ...res.data.latestStatus }];
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      this.lastUpdate = new Date().toLocaleString();
+      this.loading = false;
+    },
+
+    addPackage() {
+      this.packages = [...this.packages, this.trackingId];
+      this.trackingId = "";
+      this.savePackages();
+      this.initialize();
+    },
+
+    removePackage(index) {
+      this.packages.splice(index, 1);
+      this.savePackages();
+      this.initialize();
+    },
+
+    savePackages() {
+      localStorage.setItem("familyMartPackages", JSON.stringify(this.packages));
+    },
+
+    loadPackages() {
+      if (localStorage.getItem("familyMartPackages").length !== null) {
+        return JSON.parse(localStorage.getItem("familyMartPackages"));
+      } else {
+        return [];
+      }
+    },
+  },
+
+  computed: {
+    familyMartTrackingUrl: function () {
+      return `https://ecfme.fme.com.tw/fmedcfpwebv2/index.aspx`;
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,19 @@
   <div class="grid-container">
     <CalendarClock class="clock-block" />
     <TwitterStatistics class="twitter-block" />
+    <FamilyMartTracking class="family-mart-block" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import TwitterStatistics from "@/components/blocks/TwitterStatistics.vue";
+import FamilyMartTracking from "@/components/blocks/FamilyMartTracking.vue";
 import CalendarClock from "@/components/blocks/CalendarClock.vue";
 
 export default {
   name: "HomeView",
-  components: { TwitterStatistics, CalendarClock },
+  components: { TwitterStatistics, FamilyMartTracking, CalendarClock },
 
   computed: {},
 };
@@ -24,15 +26,19 @@ export default {
   height: 290px;
   padding: 0.6rem;
   gap: 0.6rem;
-  grid-template-columns: 0.8fr 1.2fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 2;
   grid-template-areas:
-    "twitter . clock"
-    ". . clock";
+    "twitter family-mart-block clock"
+    "twitter family-mart-block clock";
 }
 
 .twitter-block {
   grid-area: twitter;
+}
+
+.family-mart-block {
+  grid-area: family-mart-block;
 }
 
 .clock-block {
