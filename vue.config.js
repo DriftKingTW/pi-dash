f95b894d7e85acdbcdc2f50db8d8c35ff3d4cd57
DriftKingTW <driftkingtw@gmail.com>
2022-07-21 01:07:38 +0800

    build: remove deploy config for local deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/pi-dash/" : "/",
 });
