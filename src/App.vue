10e589de780c04de30a52d463d944b5daf4f0cba
DriftKingTW <driftkingtw@gmail.com>
2022-07-28 10:41:16 +0800

    feat: add pwa update notification

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,10 @@
       </v-dialog>
     </v-main>
     <SnackBar />
+    <v-snackbar bottom right :value="updateExists" :timeout="0" color="primary">
+      An update is available
+      <v-btn text @click="refreshApp"> Update </v-btn>
+    </v-snackbar>
   </v-app>
 </template>
 
@@ -61,8 +65,23 @@ export default {
 
   data() {
     return {
+      registration: null,
+      updateExists: false,
       theme: "hg-theme-default dark-theme",
     };
+  },
+
+  created() {
+    document.addEventListener("swUpdated", this.updateAvailable, {
+      once: true,
+    });
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+      // We'll also need to add 'refreshing' to our data originally set to false.
+      if (this.refreshing) return;
+      this.refreshing = true;
+      // Here the actual reload of the page occurs
+      window.location.reload();
+    });
   },
 
   mounted() {
@@ -85,6 +104,19 @@ export default {
 
     onInputChange(input) {
       this.input = input.target.value;
+    },
+
+    updateAvailable(event) {
+      this.registration = event.detail;
+      this.updateExists = true;
+    },
+
+    refreshApp() {
+      this.updateExists = false;
+      // Make sure we only send a 'skip waiting' message if the SW is waiting
+      if (!this.registration || !this.registration.waiting) return;
+      // Send message to SW to skip the waiting and activate the new SW
+      this.registration.waiting.postMessage({ type: "SKIP_WAITING" });
     },
   },
 
